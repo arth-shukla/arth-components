6e463fef828977ea8e74796b195b9652fde2ef37
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2022-08-06 20:00:07 -0700

    update class names and ids for DarkModeSwitch

--- src/components/DarkModeSwitch/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch/DarkModeSwitch.tsx
@@ -58,10 +58,10 @@ function DarkModeSwitch({
 					cx='15'
 					cy='15'
 					r={isMoon ? 12 : 6}
-					id='moon'
-					mask='url(#moon-mask)'
+					id='ai-dark-mode-switch-icon-moon'
+					mask='url(#ai-dark-mode-switch-icon-moon-mask)'
 				/>
-				<mask id='moon-mask'>
+				<mask id='ai-dark-mode-switch-icon-moon-mask'>
 					<rect
 						x='0'
 						y='0'
@@ -74,49 +74,49 @@ function DarkModeSwitch({
 						r='12'
 					/>
 				</mask>
-				<g mask='url(#dots-mask)'>
+				<g mask='url(#ai-dark-mode-switch-icon-dots-mask)'>
 					{/* top */}
 					<circle
 						cx='5'
 						cy='15'
 						r='2'
-						id='spot-1'
+						id='ai-dark-mode-switch-icon-spot-1'
 					/>
 					{/* left */}
 					<circle
 						cx='10'
 						cy='23.66'
 						r='2'
-						id='spot-2'
+						id='ai-dark-mode-switch-icon-spot-2'
 					/>
 					<circle
 						cx='20'
 						cy='23.66'
 						r='2'
-						id='spot-3'
+						id='ai-dark-mode-switch-icon-spot-3'
 					/>
 					{/* bottom */}
 					<circle
 						cx='25'
 						cy='15'
 						r='2'
-						id='spot-4'
+						id='ai-dark-mode-switch-icon-spot-4'
 					/>
 					{/* right */}
 					<circle
 						cx='10'
 						cy='6.4'
 						r='2'
-						id='spot-6'
+						id='ai-dark-mode-switch-icon-spot-6'
 					/>
 					<circle
 						cx='20'
 						cy='6.4'
 						r='2'
-						id='spot-5'
+						id='ai-dark-mode-switch-icon-spot-5'
 					/>
 				</g>
-				<mask id='dots-mask'>
+				<mask id='ai-dark-mode-switch-icon-dots-mask'>
 					<rect
 						x='0'
 						y='0'
