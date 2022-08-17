b73341ab2791dbf732634a4dd2f7e72b03c607a5
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2022-08-16 22:56:50 -0700

    full-build

---
+++ dist/cjs/components/ColorSwitch/ColorSwitch.stories.d.ts
@@ -0,0 +1,5 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import ColorSwitch from './ColorSwitch';
+declare const _default: ComponentMeta<typeof ColorSwitch>;
+export default _default;
+export declare const Button: ComponentStory<typeof ColorSwitch>;
---
+++ dist/cjs/components/DarkModeSwitch/DarkModeSwitch.stories.d.ts
@@ -0,0 +1,5 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import DarkModeSwitch from './DarkModeSwitch';
+declare const _default: ComponentMeta<typeof DarkModeSwitch>;
+export default _default;
+export declare const Button: ComponentStory<typeof DarkModeSwitch>;
