9401d7c47fbf3c4618ca842951558f966ab34776
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2022-09-02 23:35:08 -0700

    remove sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -15,12 +15,12 @@ export default [
 			{
 				file: packageJson.main,
 				format: 'cjs',
-				sourcemap: true,
+				sourcemap: false,
 			},
 			{
 				file: packageJson.module,
 				format: 'esm',
-				sourcemap: true,
+				sourcemap: false,
 			},
 		],
 		plugins: [external(), resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), postcss(), terser()],
